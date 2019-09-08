d3e3cfd4954fca0b35cb0b1d9f03b2326708f416
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-09-07 21:00:22 -0600

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "ListaEnlazada.h"
+
+using namespace std;
+
+int main() {
+
+	ListaEnlazada lista;
+	lista.agregar(1);
+	lista.agregar(2);
+	lista.agregar(4);
+	lista.agregar(5);
+	lista.imprimir();
+
+
+	//cout << *apt;
+	return 0;
+}
